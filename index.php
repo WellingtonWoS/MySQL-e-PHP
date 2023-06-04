99b8d4efa6512f9deedf075d4655dc11d7ec224f
WellingtonWoS <106634213+WellingtonWoS@users.noreply.github.com>
2023-06-03 22:23:28 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Empresa</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+               <div class="jumbotron">
+                <h1 class="display-4">Cadastro WEB</h1>
+                <p class="lead">Sistema Simplicado de Cadastro - Base de Estudo para criação de Sistemas WEB com PHP e MySQL</p>
+                <hr class="my-4">
+                <p>Acesse as funções</p>
+                <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastro</a>
+                <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">Pesquisar</a>
+               </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+    
+
+    
+  </body>
+</html>
