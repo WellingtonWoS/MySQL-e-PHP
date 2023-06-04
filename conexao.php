5960b8dad1516bd08b77d98b0a42c168cf8e3fa7
WellingtonWoS <106634213+WellingtonWoS@users.noreply.github.com>
2023-06-03 22:24:49 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,26 @@
+<?php
+
+    $server = "127.0.0.1:3312";
+    $user = "root";
+    $pass = "";
+    $bd = "empresa";
+
+    if($conn = mysqli_connect($server, $user, $pass, $bd)){
+       // echo "Conectado";
+    } else
+        echo "Erro";
+
+     function mensagem($texto, $tipo){
+        echo "<div class='alert alert-$tipo' role='alert'>
+        $texto!
+      </div>";
+     }
+    
+     function mostra_data($data){
+      $d = explode('-', $data);
+      $escreve = $d[2] ."/".$d[1]. "/".$d[0];
+      return $escreve;
+     }
+
+
+?>
